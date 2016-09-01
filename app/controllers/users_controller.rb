fa80afb7380417a51217bfcdca1613c8103c130a
Sam Bruno <sjbrunster@gmail.com>
2016-09-01 21:32:00 +0000

    Add user_stocks and listing for adding and removing tracked stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   
   def my_portfolio
+    #@user_stocks = current_user.stocks
+    @user_stocks = UserStock.where(user_id: current_user)
+    @user = current_user
   end
 
 end
